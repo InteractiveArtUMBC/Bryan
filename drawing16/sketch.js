fc1145e821260cbc8525c922343ea59d054abfbe
Bryan O'Neill <you@example.com>
2018-03-09 13:07:31 -0500

    project

---
+++ drawing16/sketch.js
@@ -0,0 +1,81 @@
+
+var osc, fft;
+
+
+
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+  frameRate(100);
+  background(0)
+  noCursor();
+  osc = new p5.TriOsc(); // set frequency and type
+  osc.amp(.5);
+
+  fft = new p5.FFT();
+  osc.start();
+}
+
+
+function draw(){
+
+  var waveform = fft.waveform();
+
+
+    var freq = map(mouseX, 0, width, 40, 880);
+    osc.freq(freq);
+
+    var amp = map(mouseY, 0, height, 1, .01);
+    osc.amp(amp);
+
+
+if (mouseIsPressed){
+      push();
+          var x = random(200,600)
+          var y = random(200,600)
+
+            fill(0)
+            ellipse(mouseX,mouseY,x,x);
+
+            var freq = map(mouseX, 0, width, 0, 200);
+            osc.freq(freq);
+
+            var amp = map(mouseY, 0, height, 5, 2);
+            osc.amp(amp);
+
+
+      pop();
+} else {
+
+  push();
+
+
+        fill(0)
+        ellipse(mouseX,mouseY,mouseX,mouseX);
+  pop();
+
+
+}
+
+
+
+
+
+  for (var i = 0; i < windowHeight; i++) {
+      var a = random(0, windowWidth);
+
+
+
+
+      var colors = ['red', 'orange',];
+      var color = random(colors);
+
+        ellipse (a,i,10,5);
+        fill(color);
+        strokeWeight(2);
+
+}
+
+
+
+}
